8c7ec88b98f938ad87ef968e9b23e9966da395d1
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2016-10-10 03:38:20 +0000

    spec jobs started

--- spec/jobs/import_goal_sheet_job_spec.rb
+++ spec/jobs/import_goal_sheet_job_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ImportGoalSheetJob do
   it 'imports excel sheet' do
-    
-
+    expect(Vision.count).to eq(0)
+    xlsx = Roo::Excelx.new('spec/fixtures/dummy_sheet.xlsx')
+    vision_sheet = xlsx.sheet('Vision')
+    vision_sheet.each(blurb: 'blurb') do |hash|
+      next if hash[:blurb] == 'blurb'
+      Vision.create(blurb: hash[:blurb])
+    end
+    expect(Vision.count).to eq(1)
   end
 end
