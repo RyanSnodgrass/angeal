d105091363a51d824f9901b40927fe224ee52b46
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2016-10-12 04:14:37 +0000

    first passing test. creates vision and goals

--- app/jobs/import_goal_sheet_job.rb
+++ app/jobs/import_goal_sheet_job.rb
@@ -1,15 +1,34 @@
+# Takes excel file and creates Vision and Goals
 class ImportGoalSheetJob
   def perform(input_file)
     @xlsx = Roo::Excelx.new(input_file)
-    vision_sheet
-
+    vision_sheet_input
+    goal_sheet_input
   end
 
-  def vision_sheet
+  def vision_sheet_input
     @vision_sheet = @xlsx.sheet('Vision')
     @vision_sheet.each(blurb: 'blurb') do |hash|
       next if hash[:blurb] == 'blurb'
       Vision.create(blurb: hash[:blurb])
+    end
+  end
+
+  def goal_ranges
+    %w(Long Medium Short)
+  end
+
+  def goal_sheet_input
+    goal_ranges.each do |range|
+      @goal_sheet = @xlsx.sheet(range)
+      @goal_sheet.each(t: 'title', wha: 'what', who: 'who', whe: 'when') do |w|
+        next if w[:t] == 'title'
+        Goal.create(title: w[:t],
+                         what: w[:wha],
+                         who: w[:who],
+                         when: w[:whe],
+                         range: range)
+      end
     end
   end
 end
--- spec/jobs/import_goal_sheet_job_spec.rb
+++ spec/jobs/import_goal_sheet_job_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ImportGoalSheetJob do
-  it 'imports excel sheet' do
-    expect(Vision.count).to eq(0)
-    xlsx = Roo::Excelx.new('spec/fixtures/dummy_sheet.xlsx')
-    vision_sheet = xlsx.sheet('Vision')
-    vision_sheet.each(blurb: 'blurb') do |hash|
-      next if hash[:blurb] == 'blurb'
-      Vision.create(blurb: hash[:blurb])
-    end
-    expect(Vision.count).to eq(1)
+  it 'creates visions and goals' do
+    expect { ImportGoalSheetJob.new.perform('spec/fixtures/dummy_sheet.xlsx') }
+      .to change { Vision.count }.from(0).to(1)
+      .and change { Goal.count }.from(0).to(5)
   end
-  it ''
+  xit 'creates association between goals and vision' do
+
+  end
 end
