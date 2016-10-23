79fe01270deeef43838da8ebb489fbc3b4ed47ad
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2016-10-23 21:57:12 +0000

    possible solution for goal tree

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,6 @@
 # Goals for all ranges of goals['Long', 'Medium', 'Short']
 class Goal < ApplicationRecord
   belongs_to :vision
+  has_many :children_goals, class_name: 'Goal', foreign_key: 'parent_goal_id'
+  belongs_to :parent_goal, class_name: 'Goal'
 end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Goal, type: :model do
     expect(g.vision).to eq(v)
     # expect(v.goals).to include(g)
   end
+
+  it 'has a many to many relationship with other goals' do
+    g = Goal.create(title: 'g')
+    g2 = Goal.create(title: 'g2')
+    g3 = Goal.create(title: 'g3')
+    g2.parent_goal = g
+    g2.children_goals << g3
+  end
 end
