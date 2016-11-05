02e9de1a18c31f241028c307d17c0d10fc69372f
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2016-11-05 23:57:49 +0000

    added comments to import job

--- app/jobs/import_goal_sheet_job.rb
+++ app/jobs/import_goal_sheet_job.rb
@@ -1,11 +1,15 @@
 # Takes excel file and creates Vision and Goals
 class ImportGoalSheetJob
+  # main method that runs the job
   def perform(input_file)
     @xlsx = Roo::Excelx.new(input_file)
     vision_sheet_input
     goal_sheet_input
   end
 
+  private
+
+  # Creates sheet object then creates vision
   def vision_sheet_input
     @vision_sheet = @xlsx.sheet('Vision').parse(header_search: [])
     @vision_sheet.drop(1).each do |row|
@@ -17,6 +21,7 @@ class ImportGoalSheetJob
     %w(Long Medium Short)
   end
 
+  # runs through each sheet creating a sheet object, then creates goals
   def goal_sheet_input
     goal_ranges.each do |range|
       @range = range
@@ -25,6 +30,7 @@ class ImportGoalSheetJob
     end
   end
 
+  # iterates through sheet, creates current goal, then ties parent-child
   def create_goals
     @goal_sheet.drop(1).each do |row|
       @row = row
@@ -34,6 +40,7 @@ class ImportGoalSheetJob
     end
   end
 
+  # Creates current goal when iterating through sheet
   def create_goal
     @current_goal_in_row = Goal.create!(title: @row['title'],
                                         what: @row['what'],
@@ -46,6 +53,7 @@ class ImportGoalSheetJob
     @vision.goals << @current_goal_in_row
   end
 
+  # finds previously created goal as the parent, then ties the current as child
   def goals_to_goals
     @higher_goal = Goal.find_by(title: @row['Higher_goal_title'])
     @higher_goal.children << @current_goal_in_row
