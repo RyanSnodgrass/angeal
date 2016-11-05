438726b365311b6ad7fe21a13e41c0b1e1ccbf43
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2016-11-05 23:52:21 +0000

    listened to depreciation warning for controller params spec

--- spec/controllers/import_controller_spec.rb
+++ spec/controllers/import_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ImportController do
       allow_any_instance_of(ImportGoalSheetJob).to receive(:perform).and_return(true)
       expect_any_instance_of(ImportGoalSheetJob).to receive(:perform)
       file = fixture_file_upload('dummy_sheet.xlsx')
-      post :import, file: file
+      post :import, params: { file: file }
       expect(response.body).to eq('Done<br><br><a href="/">Back to home</a>')
     end
   end
