a0ce4af7dd68a9f3bb8a43a26b62da82764e9bb9
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2016-11-05 14:37:00 +0000

    feeding hound

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -2,6 +2,6 @@
 class ImportController < ApplicationController
   def import
     ImportGoalSheetJob.new.perform(params[:file].path)
-    render inline: "Done<br><br><a href=\"/\">Back to home</a>"
+    render inline: 'Done<br><br><a href="/">Back to home</a>'
   end
 end
