4d7f69290977d91e7470039efad74e43db8a85a8
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2016-11-06 01:06:42 +0000

    strategic plan controller and index method added

---
+++ app/controllers/strategic_plan_controller.rb
@@ -0,0 +1,7 @@
+# methods for the general use of the app
+class StrategicPlanController < ApplicationController
+  def index
+    @vision = Vision.first
+    @goals = Goal.all
+  end
+end
---
+++ spec/controllers/strategic_plan_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe StrategicPlanController do
+  describe 'index' do
+    it 'gets the index action' do
+      get :index
+      # expect
+    end
+  end
+end
